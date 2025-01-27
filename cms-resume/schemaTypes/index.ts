14d1e27dd3c99857d55a700fd0a87870dd2dc287
Hans-Petter Eitvet <hpephoto@gmail.com>
2025-01-27 12:43:54 +0100

    Init commit

--- cms-resume/schemaTypes/index.ts
+++
@@ -1,9 +0,0 @@
-import { project } from "./project";
-import { experience } from "./experience";
-import { about } from "./about";
-
-export const schemaTypes = [
- experience,
- project,
- about,
-]
